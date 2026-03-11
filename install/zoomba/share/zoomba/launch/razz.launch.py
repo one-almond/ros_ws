5b8ea51a8b844dc12a600c756850e71ae2d3d49d
one-almond <aidancleahy01@gmail.com>
2026-03-11 14:58:17 -0400

    raspberry pi launch file built

---
+++ install/zoomba/share/zoomba/launch/razz.launch.py
@@ -0,0 +1,31 @@
+from launch import LaunchDescription
+from launch.actions import TimerAction, IncludeLaunchDescription, RegisterEventHandler
+from launch.event_handlers import OnProcessStart
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+from launch.substitutions import Command
+import os
+
+def generate_launch_description():
+    pkg_dir = get_package_share_directory('zoomba')
+    xacro_file = os.path.join(pkg_dir, 'urdf', 'zoomba.urdf.xacro')
+    rviz_config_file = os.path.join(pkg_dir, 'config', 'rviz.rviz')
+    use_sim_time = {'use_sim_time': False}
+   
+    # robot description
+    robot_state_pub = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[use_sim_time, {'robot_description': Command(['xacro ', xacro_file])}],
+    )
+
+
+
+
+    return LaunchDescription([
+        robot_state_pub,
+
+
+        ])
