d97618282e6ca0af4440fbabd162e1b1f0df4273
one-almond <aidancleahy01@gmail.com>
2026-03-11 15:16:28 -0400

    remove build install log from tracking

--- build/zoomba/CMakeFiles/zoomba_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/zoomba_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/zoomba_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
